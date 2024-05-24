a4264c22a4ff116a7443f1445f2ca99af8d676f7
Jasur Sadikov <jasur@sadikoff.com>
2024-05-24 10:00:47 +0200

    Changes and updates

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ![Demo](img.png)
 
-mud is a multidirectory git runner. Using this tool you can run git commands in a groups of repositories. This tool is not limited to git commands only, you can run any commands as you wish, but this tool was primarally designed to be used with git, so each referenced directory should have `.git`.
+mud is a multi-directory git runner. Using this tool you can run git commands in a groups of repositories. This tool is not limited to git commands only, you can run any commands as you wish, but this tool was primarily designed to be used with git, so each referenced directory should have `.git`.
 
 ## Installing
 
@@ -13,15 +13,15 @@ git clone https://github.com/jasursadikov/mud
 cd mud
 ln -s $PWD/mud.py ~/.local/bin/mud
 chmod +x ~/.local/bin/mud
-py -m venv venv
-source venv/bin/activate
+py -m venv .venv
+source .venv/bin/activate
 pip install prettytable
 ```
 
 ## Getting started
 
-1. Run `mud configure`/`mud config` to run interactive wizard which will ask you to set the preferable settings. Check section [Settings](#settings) for more. This will create `.mudsettings` file in your home directory that you can alter in future.
-2. Locate to your preferable directroy with multiple repositories.
+1. Run `mud configure`/`mud config` to run interactive wizard which will ask you to set the preferable settings. Check section [Settings](#settings) for more. This will create `.mudsettings` file in your home directory that you can alter in the future.
+2. Locate to your preferable directory with multiple repositories.
 3. Run `mud init` command to create `.mudconfig` file. This file is important to keep references to repositories. All repositories in current dictionary would be included to `.mudconfig`.
 4. Run [`mud --set-global`](#global-mudconfig) to make current configuration default and reachable from any directory.
 
@@ -35,17 +35,17 @@ All entries are stored in `.mudconfig` in XML format. After making your first en
 ### Commands
 `mud <COMMAND>` will run command on all repositories. To filter repositories check [filtering](#filters) section.
 
-- `mud labels` - displays labels for all repositores.
+- `mud labels` - displays labels for all repositories.
 - `mud log` - displays log with information about repo's last commit, it's time and it's author.
 - `mud status` - displays status in a compact table for multiple repositories.
 - `mud branch` - displays all branches in repositories.
-- `mud tags` - displays git tags for all repositores.
+- `mud tags` - displays git tags for all repositories.
 
 ### Filters
-mud has following filters:
+mud has the following filters:
 - `-l=<label>` or `--label=<label>` - filters out repositories by mud labels.
 - `-b=<branch>` or `--branch=<branch>` - filters out repositories by current branch name.
-- `-m` or `--modified` - filters out modified repostories.
+- `-m` or `--modified` - filters out modified repositories.
 - `-d` or `--diverged` - filters repositories with diverged branches.
 
 All filters should be applied before the command. 
@@ -63,12 +63,12 @@ Settings are stored in your home directory in `.mudsettings` file.
 - `config_path = /home/user/path/.mudconfig` - this is set up by `mud --set-global` [command](#global-mudconfig).
 - `nerd_fonts = 0/1` - toggles whenever nerd font icons should be used in output.
 - `auto_fetch = 0/1` - when enabled, `mud status` and `mud log` do fetch for all repos when invoked.
-- `run_async = 0/1` - enabled to run commands asyncronously.
-- `run_table = 0/1` - enable to see asyncronous commands in a table view. Requires `run_async`.
+- `run_async = 0/1` - enabled to run commands asynchronously.
+- `run_table = 0/1` - enable to see asynchronous commands in a table view. Requires `run_async`.
 - `simplify_branches = 0/1` - simplifies branch name in the branch view.
 
 ### Aliases
-You can create your own aliases for commands. To create your own aliases, edit .mudsettings file, `[alias]` section. .mudsettings has following aliases by default:
+You can create your own aliases for commands. To create your own aliases, edit .mudsettings file, `[alias]` section. .mudsettings has the following aliases by default:
 ```ini
 [alias]
 to = git checkout
--- mud.py
+++ mud.py
@@ -4,9 +4,9 @@ import os
 import sys
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
-venv_path = os.path.join(current_dir, "venv", "bin", "python")
+venv_path = os.path.join(current_dir, ".venv", "bin", "python")
 
-os.environ["PATH"] = os.pathsep.join([os.path.join(current_dir, "venv", "bin"), os.environ["PATH"]])
+os.environ["PATH"] = os.pathsep.join([os.path.join(current_dir, ".venv", "bin"), os.environ["PATH"]])
 
 if sys.executable != venv_path:
     os.execv(venv_path, [venv_path] + sys.argv)
@@ -51,32 +51,32 @@ class MudCLI:
 
     @staticmethod
     def _create_parser() -> ArgumentParser:
-        parser = argparse.ArgumentParser(description=f'mud allows you to run commands in multiple directories.')
+        parser = argparse.ArgumentParser(description=f'mud allows you to run commands in multiple repositories.')
         subparsers = parser.add_subparsers(dest='command')
 
-        subparsers.add_parser(COMMANDS['configure'][0], aliases=COMMANDS['configure'][1:], help='Run interactive configuration wizard')
-        subparsers.add_parser(COMMANDS['init'][0], aliases=COMMANDS['init'][1:], help='Initializing .mudconfig, adds all repositories in this repository to .mudconfig')
+        subparsers.add_parser(COMMANDS['configure'][0], aliases=COMMANDS['configure'][1:], help='Runs the interactive configuration wizard')
+        subparsers.add_parser(COMMANDS['init'][0], aliases=COMMANDS['init'][1:], help='Initializes the .mudconfig and adds all repositories in this directory to .mudconfig')
         subparsers.add_parser(COMMANDS['status'][0], aliases=COMMANDS['status'][1:], help='Displays git status in a table view')
         subparsers.add_parser(COMMANDS['branches'][0], aliases=COMMANDS['branches'][1:], help='Displays all branches in a table view')
-        subparsers.add_parser(COMMANDS['log'][0], aliases=COMMANDS['log'][1:], help='Displays log of last commit for all repos in a table view')
+        subparsers.add_parser(COMMANDS['log'][0], aliases=COMMANDS['log'][1:], help='Displays log of latest commit messages for all repositories in a table view')
         subparsers.add_parser(COMMANDS['labels'][0], aliases=COMMANDS['labels'][1:], help='Displays labels for all repositories')
         subparsers.add_parser(COMMANDS['tags'][0], aliases=COMMANDS['tags'][1:], help='Displays git tags for all repositories')
 
-        add_parser = subparsers.add_parser(COMMANDS['add'][0], aliases=COMMANDS['add'][1:], help='Register repository or add label to repository')
+        add_parser = subparsers.add_parser(COMMANDS['add'][0], aliases=COMMANDS['add'][1:], help='Adds repository or labels an existing repository')
         add_parser.add_argument('label', help='The label to add (optional)', nargs='?', default='', type=str)
         add_parser.add_argument('path', help='Repository to add (optional)', nargs='?', type=str)
 
-        remove_parser = subparsers.add_parser(COMMANDS['remove'][0], aliases=COMMANDS['remove'][1:], help='Remove repository or remove label from repository in .mudconfig')
+        remove_parser = subparsers.add_parser(COMMANDS['remove'][0], aliases=COMMANDS['remove'][1:], help='Removes repository or removes the label from an existing repository')
         remove_parser.add_argument('label', help='Label to remove from repository (optional)', nargs='?', default='', type=str)
         remove_parser.add_argument('path', help='Repository to remove (optional)', nargs='?', type=str)
 
-        parser.add_argument(*LABEL_PREFIX, metavar='LABEL', nargs='?', default='', type=str, help='Filter repos with provided label')
-        parser.add_argument(*BRANCH_PREFIX, metavar='BRANCH', nargs='?', default='', type=str, help='Filter repos with provided branch')
-        parser.add_argument(*MODIFIED_ATTR, action='store_true', help='Filter modified repos')
-        parser.add_argument(*DIVERGED_ATTR, action='store_true', help='Filter diverged repos')
-        parser.add_argument(COMMANDS['set-global'][0], help='Sets \'.mudconfig\' in current repository as your global \'.mudconfig\' so you can use it anywhere', action='store_true')
-        parser.add_argument(COMMANDS['version'][0], help='Displays current version of mud', action='store_true')
-        parser.add_argument('catch_all', nargs='*', help='Type any commands to execute among repositories.')
+        parser.add_argument(*LABEL_PREFIX, metavar='LABEL', nargs='?', default='', type=str, help='Filters repositories by provided label')
+        parser.add_argument(*BRANCH_PREFIX, metavar='BRANCH', nargs='?', default='', type=str, help='Filter repositories by provided branch')
+        parser.add_argument(*MODIFIED_ATTR, action='store_true', help='Filters modified repositories')
+        parser.add_argument(*DIVERGED_ATTR, action='store_true', help='Filters repositories with diverged branches')
+        parser.add_argument(COMMANDS['set-global'][0], help='Sets .mudconfig in the current repository as your fallback .mudconfig', action='store_true')
+        parser.add_argument(COMMANDS['version'][0], help='Displays the current version of mud', action='store_true')
+        parser.add_argument('catch_all', nargs='*', help='Type any commands to execute among repositories')
         return parser
 
     def run(self) -> None:
