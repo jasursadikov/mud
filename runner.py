419841506de8b5c2139bc0d539c93bcab8161616
Jasur Sadikov <jasur@sadikoff.com>
2024-09-11 10:09:29 +0200

    Spaces are fixed in no nerd fonts mode

--- runner.py
+++ runner.py
@@ -322,8 +322,8 @@ class Runner:
 	@staticmethod
 	def _print_process_header(path: str, command: str, failed: bool, code: int):
 		path = f'{BKG_BLACK}{Runner._get_formatted_path(path)}{RESET}'
-		command = f'{BKG_WHITE}{BLACK}{utils.GLYPHS[")"]} {utils.GLYPHS["terminal"]} {BOLD}{command} {RESET}{WHITE}{RESET}'
-		code = f'{WHITE}{BKG_RED if failed else BKG_GREEN}{utils.GLYPHS[")"]}{BRIGHT_WHITE} {utils.GLYPHS["failed"] if failed else utils.GLYPHS["finished"]} {f"Code: {BOLD}{code}" if failed else ""} {RESET}{RED if failed else GREEN}{utils.GLYPHS[")"]}{RESET}'
+		command = f'{BKG_WHITE}{BLACK}{utils.GLYPHS[")"]}{utils.GLYPHS["space"]}{utils.GLYPHS["terminal"]}{utils.GLYPHS["space"]}{BOLD}{command} {RESET}{WHITE}{RESET}'
+		code = f'{WHITE}{BKG_RED if failed else BKG_GREEN}{utils.GLYPHS[")"]}{BRIGHT_WHITE}{utils.GLYPHS["space"]}{utils.GLYPHS["failed"] if failed else utils.GLYPHS["finished"]} {f"Code: {BOLD}{code}" if failed else ""}{utils.GLYPHS["space"]}{RESET}{RED if failed else GREEN}{utils.GLYPHS[")"]}{RESET}'
 		print(f'{path} {command}{code}')
 
 	@staticmethod
